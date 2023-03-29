fa168d0ff3ddd5835c5730c35e56620252cdefeb
ADHL <Han.Lim.Oah@students.ittralee.ie>
2023-03-29 12:52:14 +0100

    100% code coverage is impossible

--- Han Lim Oah T00229173/test/LoanTest.java
+++ Han Lim Oah T00229173/test/LoanTest.java
@@ -1,13 +1,17 @@
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class LoanTest {
+    @Test
+    void testConstructor() {
+        Loan subject = new Loan();
+        assertNotNull(subject);
+    }
+
     @Test
     void getAmount() {
         Loan subject = new Loan(500, 1);
@@ -42,22 +46,22 @@ class LoanTest {
         Assertions.assertEquals(2, subject.getPeriod());
     }
 
-    @ParameterizedTest()
-    @CsvSource({"500,1,10"})
-    void setRate(double amount, int period, double expectedRate) throws Exception {
+    @Test
+    void setRate() throws Exception {
         Method setAmount = Loan.class.getDeclaredMethod("setAmount", double.class);
         setAmount.setAccessible(true);
         Method setRate = Loan.class.getDeclaredMethod("setRate", int.class);
         setRate.setAccessible(true);
 
         Loan subject = new Loan();
-        setAmount.invoke(subject, amount);
-        setRate.invoke(subject, period);
-        Assertions.assertEquals(expectedRate, subject.getRate());
+        setAmount.invoke(subject, 500);
+        setRate.invoke(subject, 1);
+        Assertions.assertEquals(10, subject.getRate());
     }
 
     @ParameterizedTest(name = "PT{index}: Amount = {0}, Period = {1}")
     @CsvFileSource(files = "test_rsc/LoanTestRsc/positive_test_data.csv", numLinesToSkip = 1)
+    @DisplayName("Positive Tests")
     void positiveTests(double amount, int period, double expectedRate, double expectedMonthlyPayment, double expectedTotalPayment) {
         Loan subject = new Loan(amount, period);
         double actual = subject.getRate();
@@ -70,6 +74,7 @@ class LoanTest {
 
     @ParameterizedTest(name = "NT{index}: Amount = {0}, Period = {1}")
     @CsvFileSource(files = "test_rsc/LoanTestRsc/negative_test_data.csv", numLinesToSkip = 1)
+    @DisplayName("Negative Tests")
     void negativeTests(double amount, int period) {
         assertThrowsExactly(
                 IllegalArgumentException.class,
